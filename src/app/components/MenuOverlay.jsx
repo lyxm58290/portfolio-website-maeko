429f5beaa3c55b70f87f0b3360e0d8338a8d6250
Maeko L <loomaeko@gmail.com>
2025-09-03 10:47:36 +0800

    Updated website

--- src/app/components/MenuOverlay.jsx
+++ src/app/components/MenuOverlay.jsx
@@ -3,13 +3,16 @@ import NavLink from "./NavLink";
 
 const MenuOverlay =({ links }) => {
     return(
-        <ul className="flex flex-col py-4 items-center">
-            {links.map((link, index) => (
-                <li key={index}>
-                    <NavLink href={link.path} title={link.title} />
-                </li>
-            ))}
-        </ul>
+        <div className="bg-[#4A2C17] md:hidden absolute top-full left-0 right-0 z-20">
+            <ul className="flex flex-col py-4 items-center">
+                {links.map((link, index) => (
+                    <li key={index}>
+                        <NavLink href={link.path} title={link.title} />
+                    </li>
+                ))}
+            </ul>
+        </div>
+        
     );
 };
 
