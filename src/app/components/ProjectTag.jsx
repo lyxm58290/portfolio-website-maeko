adc83d618e16c0dc3b6e9bee347940d39a262d21
Maeko L <loomaeko@gmail.com>
2025-09-03 10:52:47 +0800

    Changed project tab colour

--- src/app/components/ProjectTag.jsx
+++ src/app/components/ProjectTag.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const ProjectTag =({ name, onClick, isSelected }) => {
     const buttonStyles = isSelected
     ? "text-[#FFFEF7] border-[#9CAF88]"
-    : "text-[#ADB7BE] border-slate-600 hover:border-white"
+    : "text-[#ADB7BE] border-slate-600 hover:border-[#6D4C41]"
     return(
         <button 
             className={`${buttonStyles} rounded-full border-3 px-6 py-3 text-xl cursor-pointer`}
